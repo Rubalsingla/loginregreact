0412483e260659bb9fdd9161fc112752ef3bcbc6
Rubalsingla <89590405+Rubalsingla@users.noreply.github.com>
2022-06-11 17:05:51 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,6 +25,7 @@ const Navbar=()=>{
                         <li><Link to="/contact">Contact</Link></li>
                         <li><Link to="/login">Login</Link></li>
                         <li><Link to="/register">Register</Link></li>
+                            
                     
                     </ul>
                 </div>
